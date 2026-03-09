3a381eed7e864f419c8b22d6973840fbd374e22b
unknown <manij472@gmail.com>
2026-03-09 20:36:24 +0530

    newvalue

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,12 @@ const app = express();
 // middleware
 app.use(express.json());
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true
+// }));
 
+app.use(cors())
 // connect DB
 connectDB();
     
